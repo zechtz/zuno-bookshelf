23ed710cc7b07e623e45fda3955d33168cfbcd6a
Zachariah Ngonyani <zachariahn@gmail.com>
2023-03-19 08:48:39 +0300

    refactor app component, remove default code created by vite

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import TopHeaderContainer from "@/components/containers/TopHeaderContainer.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="wrapper">
+    <TopHeaderContainer />
+    <div class="book-container">
+      <BookSearch />
+      <BookList />
+    </div>
+    <Footer />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style scoped lang="scss">
+.wrapper {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.book-container {
+  margin-top: 200px;
+  width: 80%;
+  position: relative;
 }
 </style>
