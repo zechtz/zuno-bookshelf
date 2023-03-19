4db3dbcccc77b5a29df98179b4d18be972372178
Zachariah Ngonyani <zachariahn@gmail.com>
2023-03-19 11:25:41 +0300

    configure vite for global scss variables

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/index.scss" as *;`,
+      },
+    },
+  },
   plugins: [vue()],
 });
