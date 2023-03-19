0200fcca36c45c54f9dad4ba378ad10363f3c084
Zachariah Ngonyani <zachariahn@gmail.com>
2023-03-19 11:28:20 +0300

    WIP: finish first iteration for displaying books

---
+++ src/components/BookList.vue
@@ -0,0 +1,294 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="card-grid">
+
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>A Tale of Two Frameworks</h2>
+        <div class="card-footer">
+          <span class="book-author">O'Rayan, Elle</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>5.0</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>Peace, Love & Harmony</h2>
+        <div class="card-footer">
+          <span class="book-author">Scott, Jerry 1995</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>3.8</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>The Sand Castle Explained</h2>
+        <div class="card-footer">
+          <span class="book-author">Olson, Kayla</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>4.0</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>Lone Wolf</h2>
+        <div class="card-footer">
+          <span class="book-author">Picoult, Jodi</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>3.0</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>The English Daughter</h2>
+        <div class="card-footer">
+          <span class="book-author">Wadey, Maggie</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>4.5</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>A Tale of Two Frameworks</h2>
+        <div class="card-footer">
+          <span class="book-author">O'Rayan, Elle</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>5.0</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>Peace, Love & Harmony</h2>
+        <div class="card-footer">
+          <span class="book-author">Scott, Jerry 1995</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>3.8</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>The Sand Castle Explained</h2>
+        <div class="card-footer">
+          <span class="book-author">Olson, Kayla</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>4.0</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>Lone Wolf</h2>
+        <div class="card-footer">
+          <span class="book-author">Picoult, Jodi</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>3.0</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>The English Daughter</h2>
+        <div class="card-footer">
+          <span class="book-author">Wadey, Maggie</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>4.5</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>A Tale of Two Frameworks</h2>
+        <div class="card-footer">
+          <span class="book-author">O'Rayan, Elle</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>5.0</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>Peace, Love & Harmony</h2>
+        <div class="card-footer">
+          <span class="book-author">Scott, Jerry 1995</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>3.8</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>The Sand Castle Explained</h2>
+        <div class="card-footer">
+          <span class="book-author">Olson, Kayla</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>4.0</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>Lone Wolf</h2>
+        <div class="card-footer">
+          <span class="book-author">Picoult, Jodi</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>3.0</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <img src="@/assets/images/book-cover.png" alt="Image description" />
+      <div class="card-info">
+        <h2>The English Daughter</h2>
+        <div class="card-footer">
+          <span class="book-author">Wadey, Maggie</span>
+          <div class="rating">
+            <i class="fas fa-star"></i>
+            <span>4.5</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+$star-color: #f3d23e;
+
+.card-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(16%, 1fr));
+  gap: 1rem;
+  position: relative;
+  width: 100%;
+  margin-bottom: 100px;
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    box-sizing: border-box;
+
+    img {
+      max-width: 100%;
+      border: 1px solid #dfdfdf;
+      width: 100%;
+      height: 283px;
+    }
+
+    .card-info {
+      width: 100%;
+      text-align: left;
+      color: #000;
+      font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+        sans-serif;
+
+      h2 {
+        font-size: 25px;
+        color: #1e1e1e;
+        text-align: left;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 30px;
+      }
+
+      .book-author {
+        font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+          sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 23px;
+
+        color: rgba(0, 0, 0, 0.68);
+      }
+
+      .card-footer {
+        position: relative;
+        width: 216px;
+        height: 92px;
+        top: -15px;
+        font-size: 18px;
+        font-weight: bold;
+        color: #595858;
+
+        .rating {
+          display: flex;
+          align-items: center;
+          position: relative;
+          top: 10px;
+
+          i {
+            font-size: 24px;
+            color: $star-color;
+            cursor: pointer;
+          }
+
+          span {
+            margin-left: 8px;
+            font-size: 20px;
+            font-weight: 400;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
