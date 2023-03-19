9f48e66fd4101cb323bce50c1174d57468cd5b6a
Zachariah Ngonyani <zachariahn@gmail.com>
2023-03-19 08:48:11 +0300

    create topheadear container component

---
+++ src/components/containers/TopHeaderContainer.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Nav from "@/components/Nav.vue";
+import Cta from "@/components/Cta.vue";
+import BookSlides from "@/components/BookSlides.vue";
+</script>
+
+<template>
+  <div class="container-wrapper">
+    <Nav />
+    <Cta />
+    <!-- <BookSlides /> -->
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container-wrapper {
+  height: 70vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #eae6d7;
+}
+</style>
