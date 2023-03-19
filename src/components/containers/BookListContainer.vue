5687d0d17d10277bb769a86c14e8b4cb675c4432
Zachariah Ngonyani <zachariahn@gmail.com>
2023-03-19 20:47:10 +0300

    move logic to filter books to booklist container

--- src/components/containers/BookListContainer.vue
+++ src/components/containers/BookListContainer.vue
@@ -1,16 +1,58 @@
 <script setup lang="ts">
-import BookList from "../BookList.vue";
-import BookSearch from "../BookSearch.vue";
+import { BookType, getCheckoutData } from "@/data/service";
+import BookList from "@/components/BookList.vue";
+import BookSearch from "@/components/BookSearch.vue";
+import { Book, getMedia } from "@/data/service";
+import { computed, onMounted, reactive } from "vue";
+
+interface Data {
+  books: Array<Book>;
+}
+const data: Data = reactive({
+  books: [] as Array<Book>,
+});
+
+onMounted(() => {
+  const media = getMedia();
+  data.books = media;
+});
+
+const filteredData = computed(() => {
+  return data.books.splice(0, 15);
+});
+
+// NOTE: async this when fetching data from API
+/**
+ * Assumption is the only way to filter books by type is thorough checkout data
+ * @param category: BookType
+ * @return Array<Book>
+ */
+const filterData = (category: BookType) => {
+  const checkedData = getCheckoutData();
+  const entries = getMedia();
+
+  const checkoutData = checkedData.find(
+    (_data) => _data.ItemType === category.Code
+  );
+
+  let results: Array<Book> = [];
+
+  if (checkoutData) {
+    results = entries.filter(
+      (entry) => entry.ItemType === checkoutData.ItemType
+    );
+  }
+
+  data.books = results;
+};
 </script>
 
 <template>
   <div class="book-wrapper">
-    <TopHeaderContainer />
     <div class="book-container">
-      <BookSearch />
-      <BookList />
+      <BookSearch @option-selected="filterData" />
+      <BookList :entries="filteredData" />
     </div>
-    <Footer />
   </div>
 </template>
 
