1370394075d86491c9d7aa74bbc2fddb9aeff712
Zachariah Ngonyani <zachariahn@gmail.com>
2023-03-19 18:57:30 +0300

    WIP: remove reactvie data from app component for now

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,9 @@
 import TopHeaderContainer from "@/components/containers/TopHeaderContainer.vue";
 import BookListContainer from "@/components/containers/BookListContainer.vue";
 import FooterContainer from "@/components/containers/FooterContainer.vue";
-import { onMounted, reactive } from "vue";
-import { getMedia } from "@/data/service";
 
 interface Data {
-  books: Array<Book>;
 }
-const data: Data = reactive({
-  books: [],
-});
-
-onMounted(() => {
-  const media = getMedia();
-  console.log("media", media.default);
-  data.books = media;
-});
 </script>
 
 <template>
