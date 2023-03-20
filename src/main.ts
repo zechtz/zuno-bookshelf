20b2ca33058222be46a1fe9a1200716f05f46691
Zachariah Ngonyani <zachariahn@gmail.com>
2023-03-20 08:18:10 +0300

    organize imports in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
-import "./style.css";
-import App from "@/App.vue";
+import "@/style.css";
+import App from "components/App.vue";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 
