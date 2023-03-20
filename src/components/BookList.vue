e0f1cb96ad29033c7670732043a5dbbf417be4b8
Zachariah Ngonyani <zachariahn@gmail.com>
2023-03-20 14:20:04 +0300

    break down the booklist component, spin out a new component to handle extra data/display responsibility

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Book } from "@/data/service";
+import BookItem from "components/BookItem.vue";
 
 const props = defineProps({
   entries: {
@@ -12,30 +13,13 @@ const props = defineProps({
 <template>
   <div class="book-list-intro">
     <div class="icon">
-      <img
-        class="img"
-        src="@/assets/images/heroiconssolidbookopen1388-sgru.svg"
-      />
+      <img class="img" src="@/assets/images/heroiconssolidbookopen1388-sgru.svg" />
       <h2 class="intro-text">Book Shelf</h2>
     </div>
   </div>
   <div class="card-grid">
     <div class="card" v-for="book in props.entries" :key="book.BibNum">
-      <div class="bookmark-container">Bookmark</div>
-      <img src="@/assets/images/book-cover.png" alt="Image description" />
-      <div class="card-info">
-        <h2>{{ book.Title }}</h2>
-        <div class="card-footer">
-          <span class="book-author">{{ book.Author }}</span>
-          <div class="rating">
-            <img
-              src="@/assets/images/heroiconssolidstar1344-r5uc.svg"
-              alt="book.Title"
-            />
-            <span>5.0</span>
-          </div>
-        </div>
-      </div>
+      <BookItem :book="book" />
     </div>
   </div>
 </template>
@@ -92,73 +76,6 @@ $star-color: #f3d23e;
       border: 1px solid #dfdfdf;
       width: 240px;
       height: 283px;
-    }
-
-    .bookmark-container {
-      display: flex;
-      justify-content: end;
-      align-items: end;
-      position: absolute;
-    }
-
-    .card-info {
-      width: 100%;
-      text-align: left;
-      color: #000;
-      font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
-        sans-serif;
-
-      h2 {
-        font-size: 25px;
-        color: #1e1e1e;
-        text-align: left;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        font-style: normal;
-        font-weight: 600;
-        line-height: 30px;
-      }
-
-      .book-author {
-        font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
-          sans-serif;
-        font-style: normal;
-        font-weight: 400;
-        font-size: 20px;
-        line-height: 23px;
-
-        color: rgba(0, 0, 0, 0.68);
-      }
-
-      .card-footer {
-        position: relative;
-        width: 216px;
-        height: 92px;
-        top: -15px;
-        font-size: 18px;
-        font-weight: bold;
-        color: #595858;
-
-        .rating {
-          display: flex;
-          align-items: center;
-          position: relative;
-          top: 10px;
-
-          img {
-            width: 25px;
-            height: 25px;
-            cursor: pointer;
-          }
-
-          span {
-            margin-left: 8px;
-            font-size: 20px;
-            font-weight: 400;
-          }
-        }
-      }
     }
   }
 }
